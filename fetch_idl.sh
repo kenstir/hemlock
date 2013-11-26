b52e6824f53017f46f5846cca5869385c6f41ff9
kenstir <kenstir@gmail.com>
2013-11-26 18:09:49 -0500

    regenerated IDL and a script to fetch it.  Makes me think I better fetch it at run-time some-time in the future.

---
+++ fetch_idl.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -o assets/fm_IDL.xml 'http://bark.cwmars.org/reports/fm_IDL.xml?class=acn&class=acp&class=ahr&class=ahtc&class=au&class=cbreb&class=cbrebi&class=cbrebin&class=cbrebn&class=ccs&class=circ&class=ex&class=mbt&class=mbts&class=mous&class=mus&class=mvr&class=perm_ex'
