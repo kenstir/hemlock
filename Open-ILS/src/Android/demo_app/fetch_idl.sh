07c5740d9ec05a1e3b5f76bd0f94192e68f04a42
kenstir <kenstir@gmail.com>
2013-12-10 22:34:15 -0500

    2nd custom app is working now that it has its own fm_IDL.xml downloaded from its own site.  It differs subtly between installations (dang).

---
+++ Open-ILS/src/Android/demo_app/fetch_idl.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -o assets/fm_IDL.xml 'http://demo.evergreencatalog.com/reports/fm_IDL.xml?class=acn&class=acp&class=ahr&class=ahtc&class=au&class=cbreb&class=cbrebi&class=cbrebin&class=cbrebn&class=ccs&class=circ&class=ex&class=mbt&class=mbts&class=mous&class=mus&class=mvr&class=perm_ex'
