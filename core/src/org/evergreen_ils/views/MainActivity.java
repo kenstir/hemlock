a468e359b9f5b297df7720fed88b8f690e0b8e7f
kenstir <kenstir@gmail.com>
2013-12-29 23:17:06 -0500

    Search icon in action bar now starts SearchCatalogListView activity.  Searching works, but only in portrait mode; landscape is broken.

--- core/src/org/evergreen_ils/views/MainActivity.java
+++ core/src/org/evergreen_ils/views/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import org.evergreen_ils.R;
@@ -14,6 +17,7 @@ import org.evergreen_ils.accountAccess.checkout.ItemsCheckOutListView;
 import org.evergreen_ils.accountAccess.fines.FinesActivity;
 import org.evergreen_ils.accountAccess.holds.HoldsListView;
 import org.evergreen_ils.globals.GlobalConfigs;
+import org.evergreen_ils.searchCatalog.SearchCatalogListView;
 import org.evergreen_ils.views.splashscreen.SplashActivity;
 
 /**
@@ -40,6 +44,23 @@ public class MainActivity extends ActionBarActivity {
 
         // singleton initialize necessary IDL and Org data
         globalConfigs = GlobalConfigs.getGlobalConfigs(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+            startActivity(new Intent(getApplicationContext(), SearchCatalogListView.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void onButtonClick(View v) {
