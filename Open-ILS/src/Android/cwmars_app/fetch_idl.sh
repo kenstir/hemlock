49a51ce02724f3c07832a511639607c0451ba179
kenstir <kenstir@gmail.com>
2015-10-10 16:56:50 -0400

    The IDL is no longer packaged with the app in assets, but I am keeping this script laying around for reference.

--- Open-ILS/src/Android/cwmars_app/fetch_idl.sh
+++ Open-ILS/src/Android/cwmars_app/fetch_idl.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 root=http://bark.cwmars.org
-curl -o assets/fm_IDL.xml "$root/reports/fm_IDL.xml?class=acn&class=acp&class=ahr&class=ahtc&class=au&class=bmp&class=cbreb&class=cbrebi&class=cbrebin&class=cbrebn&class=ccs&class=circ&class=ex&class=mbt&class=mbts&class=mous&class=mus&class=mvr&class=perm_ex"
+curl -o fm_IDL.xml "$root/reports/fm_IDL.xml?class=acn&class=acp&class=ahr&class=ahtc&class=au&class=bmp&class=cbreb&class=cbrebi&class=cbrebin&class=cbrebn&class=ccs&class=circ&class=ex&class=mbt&class=mbts&class=mous&class=mus&class=mvr&class=perm_ex"
