48415550cfb1a9a476965024baa84979a56d978f
kenstir <kenstir@gmail.com>
2015-11-21 20:20:38 -0500

    From the MainActivity menu, you can switch account or add account.

--- Open-ILS/src/Android/core/src/org/evergreen_ils/views/MainActivity.java
+++ Open-ILS/src/Android/core/src/org/evergreen_ils/views/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,10 +13,12 @@ import android.view.View;
 import android.widget.Button;
 import org.evergreen_ils.R;
 import org.evergreen_ils.accountAccess.AccountAccess;
+import org.evergreen_ils.accountAccess.AccountUtils;
 import org.evergreen_ils.accountAccess.bookbags.BookbagsListView;
 import org.evergreen_ils.accountAccess.checkout.ItemsCheckOutListView;
 import org.evergreen_ils.accountAccess.fines.FinesActivity;
 import org.evergreen_ils.accountAccess.holds.HoldsListView;
+import org.evergreen_ils.auth.Const;
 import org.evergreen_ils.globals.AppPrefs;
 import org.evergreen_ils.globals.GlobalConfigs;
 import org.evergreen_ils.searchCatalog.AdvancedSearchActivity;
@@ -54,8 +57,17 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_search) {
-            startActivity(new Intent(getApplicationContext(), SearchCatalogListView.class));
+        if (id == R.id.action_switch_account) {
+            SplashActivity.restartApp(this);
+            return true;
+        } else if (id == R.id.action_add_account) {
+            AccountUtils.addAccount(this, new Runnable() {
+                @Override
+                public void run() {
+                    SplashActivity.restartApp(MainActivity.this);
+                }
+            });
+            Log.i(Const.AUTH_TAG, "after addAccount");
             return true;
         }
         return super.onOptionsItemSelected(item);
