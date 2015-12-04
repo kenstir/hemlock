276597f31f3f919bddd00e47a8dbe6747e208f8a
kenstir <kenstir@gmail.com>
2015-12-03 20:57:34 -0500

    Moved Library value obj to more reasonable home

---
+++ core/src/org/evergreen_ils/globals/Library.java
@@ -0,0 +1,23 @@
+package org.evergreen_ils.globals;
+
+import android.location.Location;
+import android.text.TextUtils;
+
+/** value class
+ * Created by kenstir on 11/5/2015.
+ */
+public class Library {
+    public String url;            // e.g. "https://catalog.cwmars.org"
+    public String name;           // e.g. "C/W MARS"
+    public String directory_name; // e.g. "Massachusetts, US (C/W MARS)"
+    public Location location;
+    public Library(String url, String name, String directory_name, Location location) {
+        this.url = url;
+        this.name = name;
+        this.directory_name = directory_name;
+        this.location = location;
+    }
+    public Library(String url, String name) {
+        this(url, name, null, null);
+    }
+}
