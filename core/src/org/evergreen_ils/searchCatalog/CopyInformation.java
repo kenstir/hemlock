75a0bcb1b2f490770f59a6c2329fe33c50b6bfbc
kenstir <kenstir@gmail.com>
2016-11-08 19:45:41 -0500

    idea cleans up imports sometimes

--- core/src/org/evergreen_ils/searchCatalog/CopyInformation.java
+++ core/src/org/evergreen_ils/searchCatalog/CopyInformation.java
@@ -24,12 +24,8 @@ import android.text.TextUtils;
 import org.evergreen_ils.system.EvergreenServer;
 
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 public class CopyInformation implements Serializable {
 
