5bb5683bad617001dd02eafc748d108f51462324
kenstir <kenstir@gmail.com>
2018-10-06 21:18:45 -0400

    Disable Pay Fines button if no fines

--- core/src/main/java/org/evergreen_ils/accountAccess/fines/FinesActivity.java
+++ core/src/main/java/org/evergreen_ils/accountAccess/fines/FinesActivity.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (C) 2012 Evergreen Open-ILS
  * @author Daniel-Octavian Rizea
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * or the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be usefull,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software 
+ * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
- * 
+ *
  */
 package org.evergreen_ils.accountAccess.fines;
 
@@ -208,6 +208,7 @@ public class FinesActivity extends BaseActivity {
                         total_owed.setText(decimalFormater.format(getFloat(finesSummary, "total_owed")));
                         total_paid.setText(decimalFormater.format(getFloat(finesSummary, "total_paid")));
                         balance_owed.setText(decimalFormater.format(getFloat(finesSummary, "balance_owed")));
+                        pay_fines_button.setEnabled(haveAnyFines);
                         progress.dismiss();
                     }
                 });
