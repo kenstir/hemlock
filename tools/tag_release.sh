e5b36a331a588f83e64ad75773d87ee6e977e6f0
kenstir <kenstir@gmail.com>
2018-10-20 15:51:19 -0400

    New tag_release script

---
+++ tools/tag_release.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "usage: $0 app_name"
+    exit 1
+fi
+app="$1"
+
+manifest="${app}_app/src/main/AndroidManifest.xml"
+if [ ! -f "$manifest" ]; then
+    echo "No such file: $manifest"
+    exit 1
+fi
+
+set -e
+
+versionCode=$(egrep android:versionCode $manifest)
+versionCode=${versionCode#*\"}
+versionCode=${versionCode%\"*}
+echo versionCode=$versionCode
+test -n "$versionCode"
+
+versionName=$(egrep android:versionName $manifest)
+versionName=${versionName#*\"}
+versionName=${versionName%\"*}
+echo versionName=$versionName
+test -n "$versionName"
+
+set -x
+
+tag=${app}_v${versionCode}
+msg="${tag} (${versionName})"
+
+git commit "$manifest" -m "$msg"
+git tag -a -m "$msg" $tag
+git push --tags
