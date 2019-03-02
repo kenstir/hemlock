16d7e14380e7e6ac067ded0a863930bff8d27f34
kenstir <kenstir@gmail.com>
2019-03-01 20:56:42 -0500

    Add class "cfg" which we see renewing a floating circ item
    To test, checkout a floating copy and try to renew it.  The response has
    an object of type "cfg".

--- core/src/main/java/org/evergreen_ils/system/EvergreenServer.java
+++ core/src/main/java/org/evergreen_ils/system/EvergreenServer.java
@@ -38,7 +38,7 @@ import java.util.*;
  */
 public class EvergreenServer {
 
-    private static final String IDL_CLASSES_USED = "ac,acn,acp,ahr,ahtc,aou,aout,au,aua,auact,aum,aus,bmp,cbreb,cbrebi,cbrebin,cbrebn,ccs,circ,csc,cuat,ex,mbt,mbts,mous,mra,mraf,mus,mvr,perm_ex";
+    private static final String IDL_CLASSES_USED = "ac,acn,acp,ahr,ahtc,aou,aout,au,aua,auact,aum,aus,bmp,cbreb,cbrebi,cbrebin,cbrebn,ccs,cfg,circ,csc,cuat,ex,mbt,mbts,mous,mra,mraf,mus,mvr,perm_ex";
 
     private static final String TAG = EvergreenServer.class.getSimpleName();
     private static EvergreenServer mInstance = null;
