117a7eacf451f1f7afc218953a1747df15c1b9f9
kenstir <kenstir@gmail.com>
2020-05-08 22:05:47 -0400

    Get URLs from MARC and trim link text

---
+++ indiana_app/src/main/java/net/kenstir/apps/indiana/IndianaAppBehavior.java
@@ -0,0 +1,70 @@
+//
+//  Copyright (C) 2019 Kenneth H. Cox
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package net.kenstir.apps.indiana;
+
+import android.text.TextUtils;
+
+import org.evergreen_ils.android.AppBehavior;
+import org.evergreen_ils.searchCatalog.RecordInfo;
+import org.evergreen_ils.utils.Link;
+import org.evergreen_ils.utils.MARCRecord;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+
+@SuppressWarnings("unused")
+public class IndianaAppBehavior extends AppBehavior {
+    private static final String TAG = IndianaAppBehavior.class.getSimpleName();
+
+    public IndianaAppBehavior() {
+        // loaded through class loader via ou_behavior_provider
+    }
+
+    @Override
+    public Boolean isOnlineResource(RecordInfo record) {
+        if (!record.basic_metadata_loaded) return null;
+        if (!record.attrs_loaded) return null;
+
+        // TODO: verify if correct
+        String item_form = record.getAttr("item_form");
+        if (TextUtils.equals(item_form, "o")
+                || TextUtils.equals(item_form, "s"))
+            return true;
+
+        return false;
+    }
+
+    // Trim the link text for a better mobile UX
+    protected String trimLinkTitle(String s) {
+        return s.replaceAll("click here", "").trim();
+    }
+
+    @Override
+    protected boolean isVisibleToOrg(MARCRecord.MARCDatafield df, String orgShortName) {
+        // Don't filter URIs because the query already did.  For a good UX we show all URIs
+        // located by the search and let the link text and the link itself control access.
+        // See also Located URIs in docs/cataloging/cataloging_electronic_resources.adoc
+        return true;
+    }
+
+    @Override @NonNull
+    public List<Link> getOnlineLocations(RecordInfo record, String orgShortName) {
+        return getOnlineLocationsFromMARC(record, orgShortName);
+    }
+}
