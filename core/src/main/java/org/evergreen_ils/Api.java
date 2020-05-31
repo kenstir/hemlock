2d1bac09a372668e50b9a084397d8b9bd76cde39
kenstir <kenstir@gmail.com>
2020-05-30 21:52:11 -0400

    Add HOURS_OF_OPERATION_RETRIEVE to API constants

--- core/src/main/java/org/evergreen_ils/Api.java
+++ core/src/main/java/org/evergreen_ils/Api.java
@@ -39,7 +39,7 @@ import java.util.List;
 public class Api {
 
     public static final String ANONYMOUS = "ANONYMOUS";
-    public static final String IDL_CLASSES_USED = "ac,acn,acp,ahr,ahtc,aou,aout,au,aua,auact,aum,aus,bmp,bre,cbreb,cbrebi,cbrebin,cbrebn,ccs,ccvm,cfg,circ,csc,cuat,ex,mbt,mbts,mous,mra,mraf,mus,mvr,perm_ex";
+    public static final String IDL_CLASSES_USED = "ac,acn,acp,ahr,ahtc,aou,aouhoo,aout,au,aua,auact,aum,aus,bmp,bre,cbreb,cbrebi,cbrebin,cbrebn,ccs,ccvm,cfg,circ,csc,cuat,ex,mbt,mbts,mous,mra,mraf,mus,mvr,perm_ex";
     public static final int LONG_TIMEOUT_MS = 10000;
 
     /// actor
@@ -62,6 +62,7 @@ public class Api {
     public static final String CONTAINER_ITEM_CREATE = "open-ils.actor.container.item.create";
     public static final String CONTAINER_FULL_DELETE = "open-ils.actor.container.full_delete";
     public static final String MESSAGES_RETRIEVE = "open-ils.actor.message.retrieve"; // [aum]
+    public static final String HOURS_OF_OPERATION_RETRIEVE = "open-ils.actor.org_unit.hours_of_operation.retrieve"; // [aouhoo]
 
     public static final String SETTING_CREDIT_PAYMENTS_ALLOW = "credit.payments.allow";
     public static final String SETTING_ORG_UNIT_NOT_PICKUP_LIB = "opac.holds.org_unit_not_pickup_lib";
