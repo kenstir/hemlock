dbd9708f00f980e35e39f23ad80007bb73039703
kenstir <kenstir@gmail.com>
2022-06-17 14:02:43 -0400

    Convert OrgArrayAdapter to kotlin
    to fix NPE

---
+++ core/src/main/java/org/evergreen_ils/utils/ui/OrgArrayAdapter.kt
@@ -0,0 +1,36 @@
+package org.evergreen_ils.utils.ui
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import org.evergreen_ils.R
+import org.evergreen_ils.system.EgOrg
+
+class OrgArrayAdapter(
+    context: Context,
+    private val resourceId: Int,
+    private val items: List<String>,
+    private val forPickup: Boolean
+) : ArrayAdapter<String>(context, resourceId, items) {
+    override fun isEnabled(pos: Int): Boolean {
+        if (!forPickup) return true
+        val org = EgOrg.visibleOrgs[pos]
+        return org.isPickupLocation
+    }
+
+    override fun getDropDownView(pos: Int, convertView: View?, parent: ViewGroup): View {
+        val v = super.getDropDownView(pos, convertView, parent)
+        val org = EgOrg.visibleOrgs[pos]
+        if (v is TextView) {
+            val canHaveUsers = org.orgType?.canHaveUsers ?: true
+            v.setTextAppearance(
+                context,
+                if (canHaveUsers) R.style.HemlockText_SpinnerSecondary else R.style.HemlockText_SpinnerPrimary
+            )
+        }
+        v.isEnabled = isEnabled(pos)
+        return v
+    }
+}
