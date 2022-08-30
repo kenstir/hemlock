kenstir/hemlock

commit 3997b5283bab767cb2a2b1c334868eefc6635996

Simplify OSRFUtils.parseInt and mark nullable
probably I should just make this trivial, like OSRFObject.getInt.
I see strings, e.g. results[count]="51", and null values, but I
have not seen anything non-Int-like.

core/src/main/java/org/evergreen_ils/OSRFUtils.java
@@ -21,6 +21,7 @@ package org.evergreen_ils;
 import android.annotation.SuppressLint;
 
 import org.evergreen_ils.android.Analytics;
+import org.evergreen_ils.android.Log;
 import org.evergreen_ils.utils.TextUtils;
 import org.opensrf.ShouldNotHappenException;
 
@@ -115,27 +116,21 @@ public class OSRFUtils {
      * Sometimes search returns a count as a json number ("count":0), sometimes a string ("count":"1103").
      * Seems to be the same for result "ids" list (See Issue #1).  Handle either form and return as an int.
      */
+    @Nullable
     public static Integer parseInt(Object o, Integer dflt) {
         if (o == null) {
             return dflt;
         } else if (o instanceof Integer) {
             return (Integer)o;
         } else if (o instanceof String) {
-            // I have seen settings with value=null, e.g. opac.default_search_location
-            if ("null".equals(o) || TextUtils.isEmpty((String) o))
-                return dflt;
-            try {
-                return Integer.parseInt((String) o);
-            } catch (NumberFormatException e) {
-                Analytics.logException(e);
-                return dflt;
-            }
+            return Integer.parseInt((String) o);
         } else {
             Analytics.logException(new ShouldNotHappenException("unexpected type: "+o));
             return dflt;
         }
     }
 
+    @Nullable
     public static Integer parseInt(Object o) {
         return parseInt(o, null);
     }

core/src/test/java/net/kenstir/apps/core/OSRFUtilsTest.kt
@@ -1,0 +1,34 @@
+/*
+ * Copyright (c) 2022 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+package net.kenstir.apps.core
+
+import org.evergreen_ils.OSRFUtils
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+import org.junit.Test
+
+class OSRFUtilsTest {
+    @Test
+    fun test_parseInt() {
+        assertNull(OSRFUtils.parseInt(null))
+
+        assertEquals(42, OSRFUtils.parseInt(Integer(42)))
+        assertEquals(42, OSRFUtils.parseInt("42"))
+    }
+}

core/src/androidTest/java/org/evergreen_ils/test/LiveServiceTest.java
@@ -239,7 +239,7 @@ public class LiveServiceTest {
             return; // search failed or server crashed
 
         Map<String, ?> response = (Map<String, ?>) resp;
-        Integer visible = OSRFUtils.parseInt(response.get("count"), 0);
+        Integer visible = OSRFUtils.parseInt(response.get("count"));
 
         // record_ids_lol is a list of lists and looks like one of:
         //   [[32673,null,"0.0"],[886843,null,"0.0"]] // integer ids+?

core/src/main/java/org/evergreen_ils/data/MBRecord.kt
@@ -137,8 +137,9 @@ class MBRecord(val id: Int, var mvrObj: OSRFObject? = null) : Serializable {
         fun makeArray(idsList: List<List<*>>): ArrayList<MBRecord> {
             val records = ArrayList<MBRecord>()
             for (i in idsList.indices) {
-                val id = OSRFUtils.parseInt(idsList[i][0])
-                records.add(MBRecord(id))
+                OSRFUtils.parseInt(idsList[i][0])?.let { id ->
+                    records.add(MBRecord(id))
+                }
             }
             return records
         }

core/src/main/java/org/evergreen_ils/system/EgSearch.kt
@@ -36,7 +36,7 @@ object EgSearch {
 
     fun loadResults(obj: OSRFObject) {
         clearResults()
-        visible = OSRFUtils.parseInt(obj["count"], 0)
+        visible = OSRFUtils.parseInt(obj["count"]) ?: 0
         if (visible == 0) return
 
         // parse ids list

core/src/main/java/org/open_ils/Event.kt
@@ -44,7 +44,7 @@ class Event : HashMap<String, Any?> {
         }
 
     val code: Int
-        get() = OSRFUtils.parseInt(get("ilsevent"), 0)
+        get() = OSRFUtils.parseInt(get("ilsevent")) ?: 0
 
     fun failed(): Boolean {
         return code != 0

core/src/test/java/net/kenstir/apps/core/OSRFObjectTest.kt
@@ -59,9 +59,14 @@ class OSRFObjectTest {
 
     @Test
     fun test_getInt() {
-        val obj = OSRFObject(mapOf<String, Any?>("id" to 42, "stringish_id" to "42"))
+        val obj = OSRFObject(mapOf<String, Any?>(
+            "id" to 42,
+            "count" to "42",
+            "null_key" to null
+        ))
         assertEquals(42, obj.getInt("id"))
-        assertEquals(42, obj.getInt("stringish_id"))
+        assertEquals(42, obj.getInt("count"))
+        assertNull(obj.getInt("null_key"))
     }
 
     @Test

commit bd8c9dc0052bf6cddfcf4dc21dc1a8b1ff32d958

Make OSRFUtils.parseInt handle "" again
It seems I broke this in commit 3997b52.  I have now
seen a patron with default_sms_carrier="" and this
issue prevented her from logging in.

core/src/main/java/org/evergreen_ils/OSRFUtils.java
@@ -123,7 +123,12 @@ public class OSRFUtils {
         } else if (o instanceof Integer) {
             return (Integer)o;
         } else if (o instanceof String) {
-            return Integer.parseInt((String) o);
+            // I have seen settings with value "", e.g. opac.default_sms_carrier
+            try {
+                return Integer.parseInt((String) o);
+            } catch (NumberFormatException e) {
+                return dflt;
+            }
         } else {
             Analytics.logException(new ShouldNotHappenException("unexpected type: "+o));
             return dflt;

core/src/test/java/net/kenstir/apps/core/OSRFUtilsTest.kt
@@ -30,5 +30,9 @@ class OSRFUtilsTest {
 
         assertEquals(42, OSRFUtils.parseInt(Integer(42)))
         assertEquals(42, OSRFUtils.parseInt("42"))
+
+        assertNull(OSRFUtils.parseInt(""))
+        assertNull(OSRFUtils.parseInt("null"))
+        assertEquals(1, OSRFUtils.parseInt("", 1))
     }
 }