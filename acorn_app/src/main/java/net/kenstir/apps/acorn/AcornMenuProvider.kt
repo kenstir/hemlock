79db1dcfd250b7c0d8a0737a7324a24401c494ce
kenstir <kenstir@gmail.com>
2023-07-28 21:18:07 -0400

    Add AcornMenuProvider in Kotlin

---
+++ acorn_app/src/main/java/net/kenstir/apps/acorn/AcornMenuProvider.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2023 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+package net.kenstir.apps.acorn
+
+import android.app.Activity
+import android.content.Intent
+import androidx.annotation.Keep
+import androidx.core.content.ContextCompat.startActivity
+import net.kenstir.apps.bibliomation.MainTilesActivity
+import net.kenstir.apps.bibliomation.R
+import org.evergreen_ils.android.Log
+import org.evergreen_ils.utils.ui.ActivityUtils
+import org.evergreen_ils.views.CheckoutsActivity
+import org.evergreen_ils.views.MenuProvider
+
+@Keep
+class AcornMenuProvider : MenuProvider() {
+    private val TAG = javaClass.simpleName
+
+    override fun onCreate(activity: Activity?) {
+        return;
+    }
+
+    override fun onItemSelected(activity: Activity?, id: Int, via: String?): Boolean {
+        when (id) {
+            R.id.main_my_account_button -> {
+                Log.d(TAG, "stop here")
+                activity?.startActivity(Intent(activity, MainTilesActivity::class.java))
+            }
+            else -> {
+                Log.d(TAG, "stop here")
+                return false
+            }
+        }
+        return true
+    }
+}
