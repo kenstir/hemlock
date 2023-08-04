adb886a002d144a15e731dd8097ac184eea6ac20
kenstir <kenstir@gmail.com>
2023-08-04 17:53:26 -0400

    Add MainGridActivity
    * If you want acorn-specific resource IDs to be visible,
      you have to import the acorn R class, not the generic one.

---
+++ acorn_app/src/main/java/net/kenstir/apps/acorn/MainGridActivity.kt
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2023 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+package net.kenstir.apps.acorn
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import net.kenstir.apps.bibliomation.R
+import org.evergreen_ils.android.App
+import org.evergreen_ils.android.Log
+import org.evergreen_ils.data.ButtonAction
+import org.evergreen_ils.data.GridButton
+import org.evergreen_ils.system.EgOrg
+import org.evergreen_ils.utils.ui.BaseActivity
+import org.evergreen_ils.views.BarcodeActivity
+import org.evergreen_ils.views.CheckoutsActivity
+import org.evergreen_ils.views.FinesActivity
+import org.evergreen_ils.views.OrgDetailsActivity
+import org.evergreen_ils.views.bookbags.BookBagsActivity
+import org.evergreen_ils.views.holds.HoldsActivity
+import org.evergreen_ils.views.search.SearchActivity
+
+class MainGridActivity : BaseActivity() {
+    private val TAG = javaClass.simpleName
+    private val SPAN_COUNT = 2
+
+    private var rv: RecyclerView? = null
+    private var adapter: GridButtonViewAdapter? = null
+    private var items = ArrayList<GridButton>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState) // not super
+        if (isRestarting) return
+
+        Log.d(TAG, object{}.javaClass.enclosingMethod?.name ?: "")
+
+        setContentView(R.layout.activity_main_grid)
+
+        rv = findViewById(R.id.recycler_view)
+        setupRecyclerView()
+        addGridButtons()
+    }
+
+    private fun setupRecyclerView() {
+        rv?.layoutManager = GridLayoutManager(this, SPAN_COUNT)
+        adapter = GridButtonViewAdapter(items)
+        rv?.adapter = adapter
+    }
+
+    private fun addGridButtons() {
+        val icon = resources.getDrawable(R.drawable.ic_barcode_48, null)
+        val b = GridButton("Digital Library Card", ButtonAction("https://www.google.com"))
+        items.add(b)
+
+        val url = EgOrg.findOrg(App.getAccount().homeOrg)?.eventsURL
+        if (!url.isNullOrEmpty()) {
+            items.add(GridButton("Events", ButtonAction(url)))
+        }
+
+//        items.add(GridButton(R.drawable.id_card_solid, "library card", "Digital Library Card", ButtonAction(activity = BarcodeActivity.java.class))
+//
+//        url = EgOrg.findOrg(App.getAccount().homeOrg)?.eventsURL
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (menuItemHandler?.onItemSelected(this, id, "main_option_menu") == true)
+            return true
+        return if (handleMenuAction(id)) true else super.onOptionsItemSelected(item)
+    }
+
+    fun onButtonClick(v: View) {
+        //throw RuntimeException("Test Crash") // test Crashlytics
+        val id = v.id
+        if (id == org.evergreen_ils.R.id.main_checkouts_button) {
+            startActivity(Intent(this, CheckoutsActivity::class.java))
+        } else if (id == org.evergreen_ils.R.id.main_holds_button) {
+            startActivity(Intent(this, HoldsActivity::class.java))
+        } else if (id == org.evergreen_ils.R.id.main_fines_button) {
+            startActivity(Intent(this, FinesActivity::class.java))
+        } else if (id == org.evergreen_ils.R.id.main_my_lists_button) {
+            startActivity(Intent(this, BookBagsActivity::class.java))
+        } else if (id == org.evergreen_ils.R.id.main_search_button) {
+            startActivity(Intent(this, SearchActivity::class.java))
+        } else if (id == org.evergreen_ils.R.id.main_library_info_button) {
+            startActivity(Intent(this, OrgDetailsActivity::class.java))
+        } else if (id == org.evergreen_ils.R.id.main_events_button) {
+            launchURL(getEventsUrl())
+        } else if (id == org.evergreen_ils.R.id.main_showcard_button) {
+            startActivity(Intent(this, BarcodeActivity::class.java))
+        } else if (menuItemHandler != null) {
+            menuItemHandler?.onItemSelected(this, id, "main_button")
+        }
+    }
+}
