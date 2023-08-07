452caafc6fe452c7a5af5a9ffa3814edafdb50e7
kenstir <kenstir@gmail.com>
2023-08-06 21:43:06 -0400

    Use updated vector assets

--- core/src/main/java/org/evergreen_ils/net/GatewayActor.kt
+++ core/src/main/java/org/evergreen_ils/net/GatewayActor.kt
@@ -89,7 +89,10 @@ object GatewayActor: ActorService {
             val settings = mutableListOf(Api.SETTING_ORG_UNIT_NOT_PICKUP_LIB,
                 Api.SETTING_CREDIT_PAYMENTS_ALLOW,
                 Api.SETTING_INFO_URL,
-                Api.SETTING_HEMLOCK_EVENTS_URL)
+                Api.SETTING_HEMLOCK_ERESOURCES_URL,
+                Api.SETTING_HEMLOCK_EVENTS_URL,
+                Api.SETTING_HEMLOCK_MEETING_ROOMS_URL,
+                Api.SETTING_HEMLOCK_MUSEUM_PASSES_URL)
             if (orgID == EgOrg.consortiumID)
                 settings.add(Api.SETTING_SMS_ENABLE)
             val args = arrayOf<Any?>(orgID, settings, Api.ANONYMOUS)
