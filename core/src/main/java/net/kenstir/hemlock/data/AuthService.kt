13e165a09d01ddd9b5f6c3138b05c9c07d12932d
kenstir <kenstir@gmail.com>
2025-06-05 14:09:21 -0400

    WIP EvergreenAuthService

---
+++ core/src/main/java/net/kenstir/hemlock/data/AuthService.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2025 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.kenstir.hemlock.data
+
+interface AuthService {
+    suspend fun login(username: String, password: String): Result<String>
+    suspend fun logout(authToken: String): Result<Unit>
+    suspend fun refreshAuthToken(oldToken: String): Result<String>
+    suspend fun validateAuthToken(authToken: String): Result<Boolean>
+}
