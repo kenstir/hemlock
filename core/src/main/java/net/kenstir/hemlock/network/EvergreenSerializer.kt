6f1e27ba0c275887cf4504d888f70be3838c429e
kenstir <kenstir@gmail.com>
2025-06-07 11:39:56 -0400

    Add kotlinx.serialization requiring kotlin2
    * switch to k2 compiler
    * upgrade to kotlin 2.1.21

---
+++ core/src/main/java/net/kenstir/hemlock/network/EvergreenSerializer.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.kenstir.hemlock.network
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+
+enum class GatewayResponseType {
+    UNKNOWN, STRING, OBJECT, ARRAY, EMPTY
+}
+
+data class GatewayResponse(
+    val status: Int,
+    @Serializable(with = EvergreenSerializer::class)
+    val payload: JsonElement,
+)
+
+// OpenSRF Gateway response serializer
+@Serializer(forClass = JsonElement::class)
+class EvergreenSerializer: KSerializer<JsonElement> {
+    override val descriptor: SerialDescriptor
+        get() = TODO("Not yet implemented")
+
+    override fun deserialize(decoder: Decoder): JsonElement {
+        TODO("Not yet implemented")
+    }
+
+    override fun serialize(encoder: Encoder, value: JsonElement) {
+        TODO("Not yet implemented")
+    }
+
+}
