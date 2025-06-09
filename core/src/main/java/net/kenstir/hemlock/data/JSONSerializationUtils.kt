012bfba011a6559cf8aa9f2582a525c11adb0347
kenstir <kenstir@gmail.com>
2025-06-09 17:05:42 -0400

    Move helper fun to JSONSerializationUtils

---
+++ core/src/main/java/net/kenstir/hemlock/data/JSONSerializationUtils.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2025 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.kenstir.hemlock.data
+
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.boolean
+import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.double
+import kotlinx.serialization.json.doubleOrNull
+import kotlinx.serialization.json.long
+import kotlinx.serialization.json.longOrNull
+
+// Helper extension to extract primitive or keep as JsonElement
+private fun JsonElement.jsonPrimitiveOrNull(): Any? = when (this) {
+    is JsonPrimitive -> when {
+        isString -> content
+        booleanOrNull != null -> boolean
+        longOrNull != null -> long
+        doubleOrNull != null -> double
+        else -> null
+    }
+    JsonNull -> null
+    else -> null
+}
