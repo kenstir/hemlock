f7708ba379705d81e53a077ca648b6e6189b0990
kenstir <kenstir@gmail.com>
2025-06-15 17:07:58 -0400

    Fix serialization of params in XGatewayClient
    * Flesh out EvergreenAuthService.login()
    * Have to use helper class GatewayParam in order to override serialization
    * Add helper fun paramListOf()
    * Use java.net class to encode Uri instead of android.net.Uri which is not mocked

--- core/src/main/java/net/kenstir/hemlock/data/evergreen/EvergreenAuthService.kt
+++ core/src/main/java/net/kenstir/hemlock/data/evergreen/EvergreenAuthService.kt
@@ -19,11 +19,14 @@ package net.kenstir.hemlock.data.evergreen
 
 import net.kenstir.hemlock.data.AuthService
 import net.kenstir.hemlock.data.Result
+import net.kenstir.hemlock.data.jsonMapOf
+import java.security.MessageDigest
 
 class EvergreenAuthService: AuthService {
     override suspend fun fetchServerVersion(): Result<String> {
         return try {
-            val json = XGatewayClient.fetch(Api.ACTOR, Api.ILS_VERSION, arrayOf(), false)
+            // shouldCache=false because this result is used as a cache-busting param
+            val json = XGatewayClient.fetch(Api.ACTOR, Api.ILS_VERSION, paramListOf(), false)
             val ret = XGatewayResult.create(json).payloadFirstAsString()
             Result.Success(ret)
         } catch (e: Exception) {
@@ -32,10 +35,54 @@ class EvergreenAuthService: AuthService {
     }
 
     override suspend fun login(username: String, password: String): Result<String> {
-        return Result.Success("AuthToken12345")
+//        return Result.Success("AuthToken12345")
+        return try {
+            // step 1: get seed
+            val json = XGatewayClient.fetch(Api.AUTH, Api.AUTH_INIT, paramListOf(username), false)
+            println("recv: $json")
+            val seed = XGatewayResult.create(json).payloadFirstAsString()
+            Result.Success(seed)
+
+            // step 2: complete auth with seed + password
+            val authParams = jsonMapOf(
+                "type" to "persist", // {opac|persist}, controls authtoken timeout
+                "username" to username,
+                "password" to md5(seed + md5(password))
+            )
+            val json2 = XGatewayClient.fetch(Api.AUTH, Api.AUTH_COMPLETE, paramListOf(authParams), false)
+            println("recv: $json2")
+
+            // step 3: get authtoken from response
+            // {"payload":[{"payload":{"authtoken":"***","authtime":1209600},"ilsevent":0,"textcode":"SUCCESS","desc":"Success"}],"status":200}
+            val obj = XGatewayResult.create(json2).payloadFirstAsObject()
+            val payload = obj.getObject("payload") ?: throw GatewayError("Missing payload in login response")
+            val authToken = payload.getString("authtoken") ?: throw GatewayError("Missing authtoken in login response")
+            //val authTime = payload.getInt("authtime") ?: throw GatewayError("Missing authtime in login  response")
+            Result.Success(authToken)
+        } catch (e: Exception) {
+            Result.Error(e)
+        }
     }
 
     override suspend fun logout(authToken: String): Result<Unit> {
         return Result.Success(Unit)
+    }
+
+    private fun md5(s: String): String {
+        val digest = MessageDigest.getInstance("MD5")
+        digest.update(s.toByteArray())
+        val messageDigest = digest.digest()
+
+        // Create Hex String
+        val hexString = StringBuilder()
+        for (i in messageDigest.indices) {
+            val hex = Integer.toHexString(0xFF and messageDigest[i].toInt())
+            if (hex.length == 1) {
+                // could use a for loop, but we're only dealing with a single byte
+                hexString.append('0')
+            }
+            hexString.append(hex)
+        }
+        return hexString.toString()
     }
 }
---
+++ core/src/main/java/net/kenstir/hemlock/data/evergreen/GatewayParam.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2025 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.kenstir.hemlock.data.evergreen
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import net.kenstir.hemlock.data.JSONDictionary
+import net.kenstir.hemlock.data.MapStringAnySerializer
+
+/** a parameter for requests through the OSRF Gateway */
+@Serializable(with = GatewayParamSerializer::class)
+data class GatewayParam(val value: Any?)
+
+fun paramListOf(vararg elements: Any?): List<GatewayParam> =
+    ArrayList(elements.map { GatewayParam(it) })
+
+object GatewayParamSerializer : kotlinx.serialization.KSerializer<GatewayParam> {
+    override val descriptor: kotlinx.serialization.descriptors.SerialDescriptor =
+        kotlinx.serialization.descriptors.PrimitiveSerialDescriptor("GatewayParam", kotlinx.serialization.descriptors.PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: kotlinx.serialization.encoding.Decoder): GatewayParam {
+        // not used
+        return GatewayParam(decoder.decodeString())
+    }
+
+    override fun serialize(encoder: kotlinx.serialization.encoding.Encoder, value: GatewayParam) {
+        when (value.value) {
+            null -> encoder.encodeNull()
+            is String -> encoder.encodeString(value.value)
+            is Int -> encoder.encodeInt(value.value)
+            is Boolean -> encoder.encodeBoolean(value.value)
+            is Double -> encoder.encodeDouble(value.value)
+            is Float -> encoder.encodeFloat(value.value)
+            is Long -> encoder.encodeLong(value.value)
+            is Map<*, *> -> {
+                val jsonDict = value.value as JSONDictionary
+                encoder.encodeSerializableValue(XOSRFObject.serializer(), XOSRFObject(jsonDict))
+            }
+            is XOSRFObject -> {
+                encoder.encodeSerializableValue(XOSRFObject.serializer(), value.value as XOSRFObject)
+            }
+            else -> throw SerializationException("Unsupported type for GatewayParam: ${value.value::class.simpleName}")
+        }
+    }
+}
