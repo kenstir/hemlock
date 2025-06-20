5b8bda837613156ec258197ca7f298784fe2b4be
kenstir <kenstir@gmail.com>
2025-06-20 09:52:14 -0400

    Move Library to net.kenstir.hemlock.data

--- core/src/main/java/net/kenstir/hemlock/android/accounts/GenericAuthenticatorActivity.kt
+++ core/src/main/java/net/kenstir/hemlock/android/accounts/GenericAuthenticatorActivity.kt
@@ -40,7 +40,7 @@ import net.kenstir.hemlock.R
 import net.kenstir.hemlock.android.AccountUtils
 import net.kenstir.hemlock.android.Analytics
 import net.kenstir.hemlock.android.Log
-import org.evergreen_ils.data.Library
+import net.kenstir.hemlock.data.Library
 import org.evergreen_ils.utils.ui.showAlert
 import java.util.Collections
 
