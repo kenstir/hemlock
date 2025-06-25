c119e7fe9b05a1ad5fcab39e84163708a7a4e2f7
kenstir <kenstir@gmail.com>
2025-06-25 14:09:19 -0400

    Remove old service method

--- core/src/main/java/org/evergreen_ils/net/PCRUDService.kt
+++ core/src/main/java/org/evergreen_ils/net/PCRUDService.kt
@@ -22,7 +22,6 @@ import org.opensrf.util.OSRFObject
 import net.kenstir.hemlock.data.Result
 
 interface PCRUDService {
-    suspend fun fetchCodedValueMaps(): Result<List<OSRFObject>>
     suspend fun fetchMARC(id: Int): Result<OSRFObject>
     suspend fun fetchMRA(id: Int): Result<OSRFObject>
     suspend fun fetchSMSCarriers(): Result<List<OSRFObject>>
