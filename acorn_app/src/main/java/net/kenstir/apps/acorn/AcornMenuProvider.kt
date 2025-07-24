0d8bd75e150449699b7311adac18d87403b643ba
kenstir <kenstir@gmail.com>
2025-07-24 14:45:39 -0400

    Update copywrite to eliminate "write to" address

--- acorn_app/src/main/java/net/kenstir/apps/acorn/AcornMenuProvider.kt
+++ acorn_app/src/main/java/net/kenstir/apps/acorn/AcornMenuProvider.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Kenneth H. Cox
+ * Copyright (c) 2025 Kenneth H. Cox
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -12,8 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
 package net.kenstir.apps.acorn
