c0dca366a743a8f4125541955ead250758dc4dc7
kenstir <kenstir@gmail.com>
2025-07-24 13:37:32 -0400

    Add MiscTest

---
+++ core/src/test/java/net/kenstir/util/MiscTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2025 Kenneth H. Cox
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+package net.kenstir.util
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class MiscTest {
+
+    private fun testString(name: String): String? {
+        return when (name) {
+            "(null)" -> null
+            "(empty)" -> ""
+            else -> name
+        }
+    }
+
+    /** Test to convince myself that I don't need TextUtils.equals() to test equality vs. null */
+    @Test
+    fun test_equals() {
+        val a = testString("hello")
+        val b = testString("hello")
+        val c = testString("(null)")
+        val d = testString("(null)")
+        val e = d
+        assertTrue(a == b)
+        assertTrue(a != c)
+        assertTrue(c != a)
+        assertTrue(c == d)
+        assertTrue(c == e)
+    }
+}
