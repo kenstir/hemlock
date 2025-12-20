2c1cdfd15f1011ba509c5ff20d59da04ffd42f25
kenstir <kenstir@gmail.com>
2025-12-20 11:29:47 -0500

    Remove commented out code

--- core/src/main/java/net/kenstir/ui/account/AccountUtilsAsync.kt
+++ core/src/main/java/net/kenstir/ui/account/AccountUtilsAsync.kt
@@ -71,10 +71,4 @@ object AccountUtilsAsync {
     ) = withContext(Dispatchers.IO) {
         AccountUtils.invalidateAuthToken(context, authToken)
     }
-
-//    suspend fun removeAllAccounts(
-//        context: Context
-//    ) = withContext(Dispatchers.IO) {
-//        throw NotImplementedError("removeAllAccounts is not yet implemented in AccountUtilsAsync")
-//    }
 }
